3f6bdf1a7c349bf6067900cd8f289ace8dddab37
Matthew <indetermattrix@gmail.com>
2025-10-11 13:24:00 +0300

    some changes in neovim configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,31 +2,48 @@
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = true
-
+vim.o.scrolloff = 8
 vim.o.swapfile = false
 
+-- visual settings
 vim.o.termguicolors = true -- ????
+vim.o.colorcolumn = "100"
+vim.o.signcolumn = "yes"
+vim.o.cursorline = false
+vim.o.showmatch = true
+vim.o.cmdheight = 0
+vim.o. = 0
 
--- tabulation
+-- tabs
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
 vim.o.expandtab = true
 
+-- search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.o.hlsearch = false
+vim.o.incsearch = true
+
 -- keymaps (basic) --
 vim.g.mapleader = " "
 
+vim.keymap.set('n', '<leader>o', ':update <cr> :so <cr>')
 vim.keymap.set('n', '<leader>w', ':write <cr>')
 vim.keymap.set('n', '<leader>q', ':quit <cr>')
 
 -- plugins --
 vim.pack.add({
+    -- UI
     { src = 'https://github.com/olimorris/onedarkpro.nvim' },
     { src = 'https://github.com/bluz71/vim-moonfly-colors' },
+    -- editor
     { src = 'https://github.com/neovim/nvim-lspconfig' },
 })
 
---colorscheme --
+-- UI --
+-- colorscheme 
 require("moonfly").custom_colors({
     bg = "#000000",
 })
@@ -39,3 +56,11 @@ vim.lsp.enable({
     'pyright',
     'lua_ls',
 })
+
+
+
+
+
+
+
+
