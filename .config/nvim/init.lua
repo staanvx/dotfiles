bf3baea6a75e4c655a5e1b9ac25d09c52eccafa5
Matthew <indetermattrix@gmail.com>
2025-10-13 00:29:36 +0300

    enable autocomplete and some lsp settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,7 +7,7 @@ vim.o.scrolloff = 8
 
 -- visual settings
 vim.o.termguicolors = true -- ????
-vim.o.colorcolumn = "100"
+vim.o.colorcolumn = "110"
 vim.o.signcolumn = "yes"
 vim.o.cursorline = false
 vim.o.showmatch = true
@@ -57,15 +57,17 @@ vim.pack.add({
     { src = 'https://github.com/bluz71/vim-moonfly-colors' },
     { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
     -- editor
-    { src = 'https://github.com/neovim/nvim-lspconfig' },
-    -- navigation
     { src = 'https://github.com/nvim-lua/plenary.nvim' },
+    { src = 'https://github.com/neovim/nvim-lspconfig' },
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
+    { src = 'https://github.com/Saghen/blink.cmp',               tag = 'v1.7.0' },
+    -- navigation
     { src = 'https://github.com/nvim-telescope/telescope.nvim' },
     { src = 'https://github.com/stevearc/oil.nvim' },
 })
 
 -- UI --
--- colorscheme 
+-- colorscheme
 require("moonfly").custom_colors({
     bg = "#000000",
 })
@@ -74,15 +76,129 @@ vim.cmd("colorscheme moonfly")
 vim.cmd("hi statusline guibg=NONE")
 
 -- editor --
+-- blink.cmp (autocomplete)
+require("blink.cmp").setup({
+    keymap = { preset = 'default' },
+
+    appearance = {
+        nerd_font_variant = 'mono',
+    },
+
+    completion = {
+        documentation = { auto_show = true },
+    },
+
+    sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+    fuzzy = {
+        implementation = "prefer_rust_with_warning",
+        prebuilt_binaries = { force_version = "v1.7.0" },
+    }
+})
+
+local capabilities = require('blink.cmp').get_lsp_capabilities()
+
 -- LSP
 vim.lsp.enable({
     'pyright',
     'lua_ls',
 })
 
+vim.lsp.config('lua_ls', {
+    capabilities = capabilities,
+    settings = {
+        Lua = { diagnostics = { globals = { 'vim' } } },
+    },
+})
+
+vim.lsp.config('pyright', {
+    capabilities = capabilities,
+})
+
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+    float = {
+        border = "rounded",
+        source = "always",
+    },
+})
+
+vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Diagnostics (float)" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev,  { desc = "Prev diagnostic" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next,  { desc = "Next diagnostic" })
+
 -- navigation --
 -- oil
-require("oil").setup()
+function _G.get_oil_winbar()
+    local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+    local dir = require("oil").get_current_dir(bufnr)
+    if dir then
+        return vim.fn.fnamemodify(dir, ":~")
+    else
+        return vim.api.nvim_buf_get_name(0)
+    end
+end
+
+require("oil").setup({
+    default_file_explorer = true,
+    -- Id is automatically added at the beginning, and name at the end
+    -- See :help oil-columns
+    columns = {
+        "permissions",
+        "icon",
+        "size",
+        "mtime",
+    },
+    buf_options = {
+        buflisted = false,
+        bufhidden = "hide",
+    },
+    win_options = {
+        wrap = false,
+        signcolumn = "no",
+        cursorcolumn = false,
+        colorcolumn = "0",
+        foldcolumn = "0",
+        spell = false,
+        list = false,
+        conceallevel = 3,
+        concealcursor = "nvic",
+        winbar = "%!v:lua.get_oil_winbar()",
+    },
+    view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+        -- This function defines what is considered a "hidden" file
+        is_hidden_file = function(name, bufnr)
+            local m = name:match("^%.")
+            return m ~= nil
+        end,
+        -- This function defines what will never be shown, even when `show_hidden` is set
+        is_always_hidden = function(name, bufnr)
+            return false
+        end,
+        -- Sort file names with numbers in a more intuitive order for humans.
+        -- Can be "fast", true, or false. "fast" will turn it off for large directories.
+        natural_order = "fast",
+        -- Sort file and directory names case insensitive
+        case_insensitive = false,
+        sort = {
+            -- sort order can be "asc" or "desc"
+            -- see :help oil-columns to see which columns are sortable
+            { "type", "asc" },
+            { "name", "asc" },
+        },
+    },
+})
+
+vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 --telescope
 
