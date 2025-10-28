144379c9abc630b156d5cf3d0431d4302775d0a1
Matthew <indetermattrix@gmail.com>
2025-10-29 01:19:51 +0300

    change colors

--- .config/scripts/tmux-vpn-status.sh
+++ .config/scripts/tmux-vpn-status.sh
@@ -3,5 +3,5 @@
 if netstat -rn | grep -qE "utun[0-9].*0/1|128.0/1"; then
     printf "#[fg=#8cc85f]󰒃 on #[default]"
 else
-    printf "#[fg=#ffcc66]󰒃 off#[default]"
+    printf "#[fg=#ff5d5d]󰒃 off#[default]"
 fi
