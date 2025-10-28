23e6f3dfae9c0e5d7df4b8ad2dda132289441337
Matthew <indetermattrix@gmail.com>
2025-10-29 00:53:56 +0300

    add spaces

--- .config/scripts/tmux-rmpc-status.sh
+++ .config/scripts/tmux-rmpc-status.sh
@@ -4,7 +4,7 @@ state=$(rmpc status | jq -r '.state' | tr '[:upper:]' '[:lower:]')
 
 case "$state" in
   play|playing)   printf " playing" ;;
-  pause|paused)   printf " paused" ;;
+  pause|paused)   printf " paused " ;;
   stop|stopped)   printf " stopped" ;;
   *)              printf ""  ;;
 esac
--- .config/scripts/tmux-vpn-status.sh
+++ .config/scripts/tmux-vpn-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if netstat -rn | grep -qE "utun[0-9].*0/1|128.0/1"; then
-    printf "#[fg=#8cc85f]󰒃#[default]"
+    printf "#[fg=#8cc85f]󰒃 on #[default]"
 else
-    printf "#[fg=#ffcc66]󰒃#[default]"
+    printf "#[fg=#ffcc66]󰒃 off#[default]"
 fi
