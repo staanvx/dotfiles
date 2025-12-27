260286c5e0dbb4f9358428b88f88cfb61de853ec
Matthew <indetermattrix@gmail.com>
2025-12-27 17:27:55 +0300

    change theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,9 @@
 local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
-local statusline = require("statusline")
+--local statusline = require("statusline")
 
-statusline.setup()
+--statusline.setup()
 
 config.enable_kitty_keyboard = false
 config.max_fps = 120
@@ -11,9 +11,9 @@ config.max_fps = 120
 config.font = wezterm.font 'Hack Nerd Font Mono'
 config.font_size = 20.0
 
-config.color_scheme = 'Moonfly (Gogh)'
+config.color_scheme = 'Catppuccin Mocha'
 config.colors = {
-  background = 'black',
+--  background = 'black',
   tab_bar = {
     background = "#1c1c1c",
 
@@ -36,14 +36,9 @@ config.colors = {
   },
 }
 
--- TODO frame
--- TODO рамка вокруг окна
--- TODO промпт для вкладки
--- TODO colors for tabs
--- TODO значок для активной вкладки
-
 config.window_close_confirmation = 'NeverPrompt'
-config.window_decorations = "RESIZE|MACOS_FORCE_SQUARE_CORNERS"
+config.window_decorations = "RESIZE"
+--config.window_decorations = "RESIZE|MACOS_FORCE_SQUARE_CORNERS"
 
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = false
@@ -55,8 +50,8 @@ config.status_update_interval = 5000
 
 
 config.window_padding = {
-  left = 2,
-  right = 2,
+  left = 5,
+  right = 5,
   top = 0,
   bottom = 0,
 }
