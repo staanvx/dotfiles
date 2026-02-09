214c11ad3dcc9496090c68440a5e440a7bc2b8f2
Matthew <indetermattrix@gmail.com>
2026-02-09 19:36:42 +0300

    workspaces

--- .config/sketchybar/plugins/space.sh
+++ .config/sketchybar/plugins/space.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-sketchybar --set "$NAME" background.drawing="$SELECTED"
+#sketchybar --set "$NAME" background.drawing="$SELECTED"
+if [ $SELECTED = true ]; then
+  sketchybar --set $NAME background.drawing=on \
+                         background.color=0xffff00ff \
+                         label.color=0xff000000 \
+                         icon.color=0xff000000
+else
+  sketchybar --set $NAME background.drawing=off \
+                         label.color=0xffffffff \
+                         icon.color=0xffffffff
+fi
